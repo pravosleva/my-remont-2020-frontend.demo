5595668fe163d92f28855219b0e3e745ff7abec9
Den <selection4test@gmail.com>
2021-02-16 23:43:43 +0300

    fix

--- src/common/mui/SiteLayout/styles.ts
+++ src/common/mui/SiteLayout/styles.ts
@@ -6,7 +6,7 @@ export const useStyles = makeStyles((theme) => ({
     backgroundColor: grey[100],
   },
   content: {
-    minHeight: 'calc(100vh - 37px - 70px)',
+    minHeight: 'calc(100vh - 37px - 60px)',
     padding: theme.spacing(1, 1, 4, 1),
     maxWidth: '1000px',
     margin: '0 auto',
