5696ad25a16bc11560c03044f46ce2784387091d
Den <selection4test@gmail.com>
2021-03-18 11:38:40 +0300

    themed select does not works (TODO)

--- src/pages/try-ui/components/ThemingSample/ThemingSample.tsx
+++ src/pages/try-ui/components/ThemingSample/ThemingSample.tsx
@@ -5,26 +5,19 @@ import {
 } from '~/common/mui/styled-partner'
 import { useBaseStyles } from '~/common/mui/baseStyles'
 import { useCustomToastContext } from '~/common/hooks'
-import { SvyaznoyMainTextField, SvyaznoyMainSelect2 } from '~/common/mui/styled-partner';
+import { SvyaznoyMainTextField } from '~/common/mui/styled-partner';
 
 export const ThemingSample = () => {
   const baseClasses = useBaseStyles()
   const { toast } = useCustomToastContext()
   // Text:
   const [textField1, setTextField1] = useState<string>('')
-  const handleChange1 = (e) => {
+  const handleChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTextField1(e.target.value)
-  }
-  // Select:
-  const items = [{ id: '01', label: 'Option 1', value: 1 }, { id: '02', label: 'Option 2', value: 2 }]
-  const [selectValue, setSelectValue] = useState<any>(items[0].value)
-  const onSelectChange = (value: any) => {
-    console.log(value)
-    setSelectValue(value)
   }
   // Btns:
   const handleClick1 = () => {
-    toast(`Hello ThemedButton for ${EPartnerCode.SvyaznoySecondary}; ${textField1 || '<empty>'}; ${selectValue}`, { appearance: 'info' })
+    toast(`Hello ThemedButton for ${EPartnerCode.SvyaznoySecondary}; ${textField1 || '<empty>'}`, { appearance: 'info' })
   }
   const handleClick2 = () => {
     toast(`Hello ThemedButton for ${EPartnerCode.SvyaznoyYellow}`, { appearance: 'success' })
@@ -33,19 +26,8 @@ export const ThemingSample = () => {
   return (
     <div className={baseClasses.subWrapper}>
       <div className={baseClasses.wrapperAsGrid}>
-        <div className={baseClasses.subWrapper}>
-          <SvyaznoyMainSelect2
-            label='SvyaznoyMainSelect'
-            // id='SvyaznoyMainSelect'
-            value={selectValue}
-            onChange={onSelectChange}
-            items={items}
-            fullWidth={true}
-            size='small'
-            // color='svyaznoy'
-            // variant="outlined"
-            // native
-          />
+        <div className={baseClasses.subWrapper} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <b>[ Something in progress... ]</b>
         </div>
         <div className={baseClasses.subWrapper} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <b>[ Something in progress... ]</b>
@@ -56,8 +38,6 @@ export const ThemingSample = () => {
         variant="outlined"
         name="field-1"
         value={textField1}
-        // @ts-ignore
-        color="svyaznoy"
         onChange={handleChange1}
         fullWidth
         size='small'
